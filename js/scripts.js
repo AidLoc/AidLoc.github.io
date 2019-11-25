d0f41cd23c9ebf4579b0bc3ef07aedbd69174be3
Aiden Locke <aidenslocke@gmail.com>
2019-11-25 09:37:05 -0500

    New U/I for stats

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
+//snapping
 $('#myRange').change(function () {
     var cVal = $(this).val();
-    console.log(cVal);
-    if (cVal <= 15) {
+    if (cVal <= 5) {
         $('#myRange').prop("value", "0");
+        myFunction()
         document.getElementById("migration_header").innerHTML = "In 1900, 2/3 of Canadians lived in rural areas.";
     }
 
-    if (cVal >= 40 && cVal <= 60) {
+    if (cVal >= 45 && cVal <= 55) {
         $('#myRange').prop("value", "50");
+        myFunction()
         document.getElementById("migration_header").innerHTML = "By 1971, 2/3 of Canadians lived in towns or cities.";
     }
 
-    if (cVal >= 85) {
+    if (cVal >= 95) {
         $('#myRange').prop("value", "100");
+        myFunction()
         document.getElementById("migration_header").innerHTML = "In 2016, 74% of Canadians live in suburbs.";
+
     }
 });
 
+
+//graphic i/o
 document.getElementById("myRange").oninput = function () {
     myFunction()
 }
@@ -29,9 +35,8 @@ function myFunction() {
         document.documentElement.style.setProperty('--Rval', g1 + '%');
         document.documentElement.style.setProperty('--Uval', g2 + '%');
 
-        document.getElementById("rural_p").innerHTML = g1 + '%';
-        
-        document.getElementById("suburban_p").innerHTML = g2 + '%';
+        document.getElementById("rural_p").innerHTML = Math.round(g2) + '%';
+        document.getElementById("suburban_p").innerHTML = Math.round(g1) + '%';
     }
 
     if (pos > 50) {
@@ -39,36 +44,132 @@ function myFunction() {
         var g2 = ((pos * -0.16) + 41);
         document.documentElement.style.setProperty('--Rval', g1 + '%');
         document.documentElement.style.setProperty('--Uval', g2 + '%');
+
+        document.getElementById("rural_p").innerHTML = Math.round(g2) + '%';
+        document.getElementById("suburban_p").innerHTML = Math.round(g1) + '%';
     }
 
 }
 
+//snapping2
+$('#myRange2').mousemove(function () {
+    var cVal2 = $(this).val();
 
+    if (cVal2 > 11.5) {
+        document.getElementById("A").setAttribute("src", "images/haus.png");
+    }
+    if (cVal2 < 11.5) {
+        document.getElementById("A").setAttribute("src", "images/tree.png");
+    }
+    if (cVal2 > 11.5) {
+        document.getElementById("G").setAttribute("src", "images/haus.png");
+    }
+    if (cVal2 < 11.5) {
+        document.getElementById("G").setAttribute("src", "images/tree.png");
+    }
+    
+    if (cVal2 > 30) {
+        document.getElementById("B").setAttribute("src", "images/haus.png");
+    }
+    if (cVal2 < 30) {
+        document.getElementById("B").setAttribute("src", "images/tree.png");
+    }
+    if (cVal2 > 30) {
+        document.getElementById("H").setAttribute("src", "images/haus.png");
+    }
+    if (cVal2 < 30) {
+        document.getElementById("H").setAttribute("src", "images/tree.png");
+    }
+    
+    
+    if (cVal2 > 50) {
+        document.getElementById("C").setAttribute("src", "images/haus.png");
+    }
+    if (cVal2 < 50) {
+        document.getElementById("C").setAttribute("src", "images/tree.png");
+    }
+    if (cVal2 > 50) {
+        document.getElementById("I").setAttribute("src", "images/haus.png");
+    }
+    if (cVal2 < 50) {
+        document.getElementById("I").setAttribute("src", "images/tree.png");
+    }
+    
+    
+    if (cVal2 > 70) {
+        document.getElementById("D").setAttribute("src", "images/haus.png");
+    }
+    if (cVal2 < 70) {
+        document.getElementById("D").setAttribute("src", "images/tree.png");
+    }
+    if (cVal2 > 70) {
+        document.getElementById("J").setAttribute("src", "images/haus.png");
+    }
+    if (cVal2 < 70) {
+        document.getElementById("J").setAttribute("src", "images/tree.png");
+    }
+    
+    
+    if (cVal2 > 90) {
+        document.getElementById("E").setAttribute("src", "images/haus.png");
+    }
+    if (cVal2 < 90) {
+        document.getElementById("E").setAttribute("src", "images/tree.png");
+    }
+    if (cVal2 > 90) {
+        document.getElementById("K").setAttribute("src", "images/haus.png");
+    }
+    if (cVal2 < 90) {
+        document.getElementById("K").setAttribute("src", "images/tree.png");
+    }
+    
+    if (cVal2 >= 100) {
+        document.getElementById("F").setAttribute("src", "images/haus.png");
+    }
+    if (cVal2 < 100) {
+        document.getElementById("F").setAttribute("src", "images/tree.png");
+    }
+    if (cVal2 >= 100) {
+        document.getElementById("L").setAttribute("src", "images/haus.png");
+    }
+    if (cVal2 < 100) {
+        document.getElementById("L").setAttribute("src", "images/tree.png");
+    }
+    
+    
+    
+    
+});
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+//graphic i/o2
+document.getElementById("myRange2").oninput = function () {
+    myFunction2()
 }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
+function myFunction2() {
+    var pos = document.getElementById("myRange2").value;
+    console.log(pos);
+    var g3 = (89.848484848485 * pos) + 5561.1515151515;
 
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+    document.getElementById("area").innerHTML = Math.round(g3);
+}
 
-    slides[slideIndex - 1].style.display = "block";
-    console.log(n);
+function status() {
+    if (document.getElementById('healthy').checked){
+        document.getElementById('statusimg').setAttribute("src", "images/healthy.png")
+        document.getElementById('effect_header').innerHTML = "Those living in rural environments have on average the lowest number of chronic medical conditions.";
+    }
+    
+    else if (document.getElementById('moderate').checked){
+        document.getElementById('statusimg').setAttribute("src", "images/moderate.png")
+        document.getElementById('effect_header').innerHTML = "Those living in urban environments have on average 2nd highest number of chronic medical conditions.";
+    }
+    
+    else if (document.getElementById('unhealthy').checked){
+        document.getElementById('statusimg').setAttribute("src", "images/unhealthy.png")
+        document.getElementById('effect_header').innerHTML = "Those living in suburban environments have on average the highest number of chronic medical conditions.";
+    }
+    
 }
 
 
@@ -87,7 +188,6 @@ $("#T2").click(function () {
         1750);
 });
 
-//document.getElementById("#T2").addEventListener("click", function(){plusSlides(1)});
 
 $("#T3").click(function () {
     $('html,body').animate({
