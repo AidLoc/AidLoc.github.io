0b53cf6b0bc2e9d244a4dbb3eaab42e70f9fb580
Aiden Locke <aidenslocke@gmail.com>
2020-01-21 12:01:40 -0500

    New Portfolio Site (temp)
    archived and relinked dependancies

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function s1() {
+    $("body").scrollTo("#B", 1000);
+};
+
+var moved = false;
+
+var active = $("#A");
+
+var unit = $(active).height() * 0.65;
+
+$(document).scroll(function () {
+    var scroll = $(document).scrollTop();
+    console.log(scroll);
+    if (scroll >= unit){
+        var moved = true;
+    }
+    
+    var moved = false;
+});
