99a94ccc43d9d514f34ff1a86c41d843940b4701
Aiden Locke <aidenslocke@gmail.com>
2020-04-23 00:03:06 -0400

    temporarily removed personal site

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,217 @@
-$(".grid-item").click(function () {
-    var item = "#" + $(this).attr('class').split(" ").pop();
-    console.log(item);
+window.togg;
 
-    if ($(".port").hasClass('togg')) {
-        $(item).removeClass('togg');
-    } else {
-        $(item).addClass('togg');
-        $(window).scrollTo(item, 2000, {
-            offset: -75
-        });
-        $(window).promise().done(function () {
-            $('.cont').css('display', 'block');
-            $('.desc').css('display', 'block');
-        });
+var home = true;
+
+$(function setup() {
+    onepagescroll('div.pages', {
+        pageContainer: 'section',
+        animationType: 'ease',
+        animationTime: 1100,
+        infinite: false,
+        pagination: true,
+        keyboard: false,
+        direction: 'horizontal'
+    });
+
+    $(".A").hide();
+    $(".B").hide();
+    $(".C").hide();
+    $(".D").hide();
+    $(".E").hide();
+    $(".F").hide();
+    $("#nav").hide();
+    $("#dbox").hide();
+    togg = false;
+    var A = $("#point").position().left;
+    var B = $(".last").position().left;
+});
+
+/*$("#point").click(function () {
+    $('body').jGravity({ 
+        target: 'img#logo', 
+        ignoreClass: 'ignoreMe', 
+        weight: 75, 
+        depth: 5, 
+        drag: true 
+    });
+});*/
+
+$(".info").click(function () {
+    togg = false;
+    console.log(currentPage);
+    window.proj;
+    if (currentPage == 1) {
+        proj = (".A");
+    } else if (currentPage == 2) {
+        proj = (".B");
+    } else if (currentPage == 3) {
+        proj = (".C");
+    } else if (currentPage == 4) {
+        proj = (".D");
+    } else if (currentPage == 5) {
+        proj = (".E");
+    } else if (currentPage == 6) {
+        proj = (".F");
+    }
+
+    $("#dbox").hide('slide', {
+        direction: "left"
+    }, 1000);
+    $("#nav").slideUp(1000);
+    $(".pages").fadeOut(1000);
+    $(proj).fadeIn(1000);
+    console.log(togg);
+});
+
+$(document).ready(function () {
+    if (currentPage == 7) {
+
     }
 });
 
-$(".close").click(function () {
-    console.log('closed');
-    var item2 = $(this).parents('.port');
-    $(item2).removeClass('togg');
-    console.log(item2);
-});
-
-$("#dev-btn").click(function () {
-    window.location.href = 'final/index.html';
-});
-
-$("#mood-btn").click(function () {
-    window.location.href = 'concept.html';
-});
-
-$("#narr-btn").click(function () {
-    window.location.href = 'Narrative.html';
-});
-
-
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    center: true,
-    autoWidth: true,
-    items: 3,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 1
+var contact = function () {
+    if (home == false && togg == true) {
+        if (currentPage == pages.length) {
+            $("#dbox").fadeOut(1000);
+            $("#nav").slideUp(1000);
+        } else {
+            $("#dbox").fadeIn(1000);
+            $("#nav").slideDown(1000);
         }
     }
-})
+}
 
+setInterval(contact, 500);
+
+
+$(".return").click(function () {
+    togg = true;
+    $("#dbox").show('slide', {
+        direction: "left"
+    }, 1000);
+    $("#nav").slideDown(1000);
+    $(".pages").fadeIn(1000);
+    $(proj).fadeOut(1000);
+    console.log(togg);
+});
+
+
+//navigation via pagination
+function navi(hr) {
+    var incr = (hr - currentPage);
+    var mult = Math.abs(incr);
+    setting.animationTime = setting.animationTime + (200*mult);
+    console.log(mult);
+    console.log(setting.animationTime);
+    if (hr > currentPage) {
+        changePage(pages.length, 1, incr);
+    } else if (hr < currentPage) {
+        changePage(1, pages.length, incr);
+    }
+    setting.animationTime = 500;
+}
+
+
+
+//homepage stuffs
+$(".drg").draggable({
+    axis: 'x',
+    containment: "#sliderbox"
+});
+
+$(".drg").mouseenter(function () {
+    $(".drg > p").fadeOut(500);
+});
+
+if (home == true) {
+    $(window).mouseup(function () {
+        A = $(".drg").position().left;
+        B = $(".last").position().left;
+        ratio = (A / B) * 100
+        W = $(window).height();
+
+        if (ratio >= 80) {
+            togg = true;
+            $(".drg").draggable('disable');
+            $(".drg").fadeOut(1250);
+            $("#nav").slideDown(1250);
+            $("#dbox").show('slide', {
+                direction: "left"
+            }, 1250);
+            $("#home").slideUp(1250);
+            $("#hbox").animate({
+                'up': W
+            }, 1250);
+            /*$('html,body').animate({
+                    scrollTop: $(".pages").offset().top
+                },
+                1750);*/
+            home = false;
+        }
+    });
+}
+
+//draggable galleries
+
+$(".right .fimg").draggable({
+    containment: "parent"
+});
+
+$(".left .fimg").draggable({
+    containment: "parent"
+});
+
+$(".lofi2 .fimg").draggable({
+    containment: "parent"
+});
+
+$(".dida1 .fimg").draggable({
+    containment: "parent"
+});
+
+$(".gall .fimg").draggable({
+    containment: "parent"
+});
+
+
+var z = 100;
+
+$(".fimg").mousedown(function () {
+    z++;
+    $(this).css("z-index", z);
+    $(this).css("animation-play-state", "paused");
+    //$(this).addClass("grow");
+});
+
+$(".fimg").mouseup(function () {
+    //$(this).removeClass("grow");
+    $(this).css("animation-play-state", "running");
+});
+
+var coll = $(".gall img");
+var item;
+
+var zrand = function () {
+    if ($(".F").is(":visible")) {
+        item = coll[Math.floor(Math.random() * coll.length)];
+        z++;
+        $(item).css("z-index", z);
+    }
+}
+
+setInterval(zrand, 1000);
+
+//preload code
 
 function load() {
     page = setTimeout(showPage, 3000);
 }
 
 function showPage() {
-    document.getElementById("cover").style.display = "none";
+    $(".preload").fadeOut(2000);
 }
+
+
+
+//$('a[href$="ABC"]')
+//var c = '#' + Math.floor(Math.random() * 16777215).toString(16);
